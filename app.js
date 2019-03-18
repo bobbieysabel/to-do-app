f0f1fa2f29ef48703053b193359640701fb2b5e0
byben <b.y.benedito@gmail.com>
2019-03-17 22:08:00 -0400

    Add delete button function

--- app.js
+++ app.js
@@ -5,31 +5,41 @@ function onReady(){
 
     addToDoForm.addEventListener('submit', () => {
         event.preventDefault();
-
-        //retrieve input text
-        let title = newToDoText.value;
-
+        
         //create list item
-        let newLi = document.createElement('li');
-
-        //create new input
+        let newLi = document.createElement('li');  
+        
+        //create and attach new checkpot input to list item
         let checkbox = document.createElement('input');
+        checkbox.type = "checkbox"; 
+        newLi.appendChild(checkbox);       
+        
+        //retrieve text input and attach to list item
+        let title = document.createTextNode(newToDoText.value);
+        newLi.appendChild(title);
 
-        //set input type to checkbox
-        checkbox.type = "checkbox";
-
-        //set title
-        newLi.textContent = title;
-
-        //attach checkbox to list item
-        newLi.appendChild(checkbox);
+        //create and attach delete button to list item
+        let btn = document.createElement('button');
+        let text = document.createTextNode('Delete');
+        btn.className = 'close';
+        btn.appendChild(text);
+        newLi.appendChild(btn);        
 
         //add list item to to-do list
         toDoList.appendChild(newLi);
 
         //empty form input
         newToDoText.value = "";
-
+        
+        //deletes list items when delete button is clicked
+        let close = document.getElementsByClassName("close");
+        for (let i = 0; i < close.length; i++) {
+          close[i].onclick = function() {
+            parent = document.getElementById("toDoList")
+            child = this.parentNode;
+            parent.removeChild(child);
+          }
+        }
     });
 }
 
