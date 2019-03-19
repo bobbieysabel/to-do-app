11214adfbd0bfe537b74182aedd71e16f187028c
byben <b.y.benedito@gmail.com>
2019-03-19 17:13:13 -0400

    Add delete button function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function onReady(){
-    const toDos = [];
     const addToDoForm = document.getElementById('addToDoForm');
+    let toDos = [];
+    let id = 0;
 
+    //creates new to do item and adds item to array
     function createNewToDo() {
         const newToDoText = document.getElementById('newToDoText');
 
@@ -9,14 +11,18 @@ function onReady(){
 
         toDos.push({
             title: newToDoText.value,
-            complete: false
+            complete: false,
+            id: id
         });
+
+        id++;
 
         newToDoText.value = '';
 
         renderTheUI();
     }
 
+    //renders UI for to do list
     function renderTheUI() {
         const toDoList = document.getElementById('toDoList');
 
@@ -26,14 +32,29 @@ function onReady(){
             const newLi = document.createElement('li');
             const checkbox = document.createElement('input');
             checkbox.type = 'checkbox';
+            
+            //creates delete button and adds click event
+            const deleteButton = document.createElement('button');
+            const deleteText = document.createTextNode('Delete');
+            deleteButton.appendChild(deleteText);
+            deleteButton.addEventListener('click', event => {
+                toDos = toDos.filter(function(item){
+                    return item.id !== toDo.id;
+                })
+
+                renderTheUI();
+            });
 
             newLi.textContent = toDo.title;
 
             toDoList.appendChild(newLi);
             newLi.appendChild(checkbox);
+
+            newLi.appendChild(deleteButton);
         });
     }
 
+    //creates new to do when form is submitted
     addToDoForm.addEventListener('submit', event => {
         event.preventDefault();
         createNewToDo();
